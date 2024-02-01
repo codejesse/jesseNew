95f0433116baf30abdc376fb76d8f3c8c5c525f8
Jesse <jessebeke10@gmail.com>
2024-02-02 00:55:17 +0100

    added animated button

--- app/page.tsx
+++ app/page.tsx
@@ -8,6 +8,15 @@ export default function Home() {
           Jesse Beke
         </p>
       </div>
+      <button className="group relative grid overflow-hidden rounded-full px-4 py-1 shadow-[0_1000px_0_0_hsl(0_0%_20%)_inset] transition-colors duration-200">
+        <span>
+          <span className="spark mask-gradient absolute inset-0 h-[100%] w-[100%] animate-flip overflow-hidden rounded-full [mask:linear-gradient(white,_transparent_50%)] before:absolute before:aspect-square before:w-[200%] before:rotate-[-90deg] before:animate-rotate before:bg-[conic-gradient(from_0deg,transparent_0_340deg,white_360deg)] before:content-[''] before:[inset:0_auto_auto_50%] before:[translate:-50%_-15%]" />
+        </span>
+        <span className="backdrop absolute inset-[1px] rounded-full bg-neutral-950 transition-colors duration-200 group-hover:bg-neutral-800" />
+        <span className="z-10 py-0.5 text-sm text-neutral-100">
+          ✨ hello there
+        </span>
+      </button>
       <Image src="/Jesse-1.svg" alt="jesse-pic" width={50} height={50} />
       <h1 className="mt-[-30px]">Hi i'm Jesse Beke</h1>
       <div className="absolute top-0 z-[-2] h-screen w-screen bg-neutral-950 bg-[radial-gradient(ellipse_80%_80%_at_50%_-20%,rgba(120,119,198,0.3),rgba(255,255,255,0))]"></div>
