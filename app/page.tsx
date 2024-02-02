bc09f75783f166bfc4f0ca52520a038cb978b621
Jesse <jessebeke10@gmail.com>
2024-02-02 01:10:29 +0100

    nav initial set-up

--- app/page.tsx
+++ app/page.tsx
@@ -2,11 +2,18 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <main className="flex min-h-screen flex-col items-center gap-4 p-24">
       <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Jesse Beke
-        </p>
+        <div className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-full lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
+          <Image src="/Jesse-1.svg" alt="jesse" width={40} height={40} />
+          <h1 className="text-center lg:m-auto p-2">Jesse Beke</h1>
+        </div>
+        {/* for icons in nav */}
+        <div className="flex flex-row gap-4">
+          <p>icon</p>
+          <p>icon</p>
+          <p>icon</p>
+        </div>
       </div>
       <button className="group relative grid overflow-hidden rounded-full px-4 py-1 shadow-[0_1000px_0_0_hsl(0_0%_20%)_inset] transition-colors duration-200">
         <span>
@@ -17,8 +24,11 @@ export default function Home() {
           ✨ hello there
         </span>
       </button>
-      <Image src="/Jesse-1.svg" alt="jesse-pic" width={50} height={50} />
-      <h1 className="mt-[-30px]">Hi i'm Jesse Beke</h1>
+      <h1 className="mb-3 text-5xl font-semibold">I'm Jesse</h1>
+      <p className="w-[80%] lg:w-[50%] text-center">
+        I'm a frontend engineer. I work with the React Ecosystem and I also
+        create content around Javascript, React, Next JS and Typescript.
+      </p>
       <div className="absolute top-0 z-[-2] h-screen w-screen bg-neutral-950 bg-[radial-gradient(ellipse_80%_80%_at_50%_-20%,rgba(120,119,198,0.3),rgba(255,255,255,0))]"></div>
       <h1 className="mb-3 text-5xl font-semibold">Under construction 🚧</h1>
       <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left"></div>
