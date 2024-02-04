797943cb1bf5cde24f4bf8e475d0cf237dda87cf
Jesse <jessebeke10@gmail.com>
2024-02-04 22:48:31 +0100

    Fixed background issue and added project section

---
+++ app/components/CardList.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+interface projectProps {
+    name: string,
+    img?: string,
+}
+
+const CardList = () => {
+  return (
+    <div>CardList</div>
+  )
+}
+
+export default CardList
