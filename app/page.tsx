6bed1a5db3deb7701064feb0509f7a3d2f412efc
Jesse <jessebeke10@gmail.com>
2024-02-04 01:23:36 +0100

    added CTA fixes

--- app/page.tsx
+++ app/page.tsx
@@ -46,7 +46,7 @@ export default function Home() {
           </p>
         </div>
         <div className="flex justify-center mt-4 flex-row">
-          <button className="rounded-md text-sm transition-colors focus-visible:outline-none disabled:pointer-events-none disabled:opacity-50 border bg-white text-primary-foreground hover:bg-primary/90 h-10 px-4 py-2 rounded-l-full font-semibold flex items-center justify-center gap-2">
+          <button className="rounded-md cursor-pointer text-sm transition-colors focus-visible:outline-none 0 border bg-white text-primary-foreground hover:bg-primary/90 h-10 px-4 py-2 rounded-l-full font-semibold flex items-center justify-center gap-2">
             <svg
               className="w-6 h-6 text-gray-800 dark:text-white"
               aria-hidden="true"
