0f7e411cdf0c64331a8eb55a8607433146c21925
Jesse <jessebeke10@gmail.com>
2024-02-13 20:17:20 +0100

    removed icon in footer because the navbar has icons and it is sticky

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -9,7 +9,7 @@ const Footer = () => {
       <div className="text-ceneter p-8 text-gray-500 text-sm lg:text-md">
         ©2024 Jesse Beke • All Rights Reserved.
       </div>
-      <div className="flex flex-row gap-2">
+      <div className="hidden flex-row gap-2">
         <GitHubIcon className="w-10 my-auto text-white" />
         <LinkedInIcon className="w-6 my-auto text-white" />
         <XIcon className="w-5 ml-2 my-auto text-white" />
