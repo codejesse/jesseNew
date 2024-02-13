8e3491894d0cf850892f3109b69a0d6dc21e3974
Jesse <jessebeke10@gmail.com>
2024-02-13 20:12:48 +0100

    footer text fix

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Footer = () => {
   return (
     <div className='flex justify-center mt-8 p-8'>
-        <div className='text-ceneter p-8 text-gray-500'>©2024 Jesse Beke • All Rights Reserved.</div>
+        <div className='text-ceneter p-8 text-gray-500 text-sm lg:text-md'>©2024 Jesse Beke • All Rights Reserved.</div>
     </div>
   )
 }
