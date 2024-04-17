d85d139a84e34bbc7bff4f96f972773b739017c1
Jesse <jessebeke10@gmail.com>
2024-04-17 13:58:48 +0100

    temp commenting our cardlist

--- app/components/CardList.tsx
+++ app/components/CardList.tsx
@@ -1,12 +1,12 @@
-import React from 'react'
-import { projects } from '../data/projects'
+// import React from 'react'
+// import { projects } from '../data/projects'
 
-const CardList = () => {
-  return (
-    <div>{projects.map((id, project) => (
-      <h1>{}</h1>
-    ))}</div>
-  )
-}
+// const CardList = () => {
+//   return (
+//     <div>{projects.map((id, project) => (
+//       <h1>{}</h1>
+//     ))}</div>
+//   )
+// }
 
-export default CardList
+// export default CardList
