1a21c240f347d9d5f98857821993026ff9f14895
Jesse <jessebeke10@gmail.com>
2024-06-08 22:53:26 +0100

    feat: added forge ai project to the list

--- app/data/projects.tsx
+++ app/data/projects.tsx
@@ -19,6 +19,15 @@ const projects: Project[] = [
     status: "In-progress",
   },
   {
+    projectName: "Forge ai",
+    imgUrl: "https://res.cloudinary.com/daiio9iqt/image/upload/v1717883437/projects/1c708318-2757-45d5-8af2-8956344ba902.png",
+    desc: "Forge ai is an ai scheduler that makes it super easy to create a personalized schedule just from a prompt, also allowing you add it to your google calendar.",
+    gitLink: "https://github.com/codejesse/forge-ai",
+    link: "https://github.com/codejesse/forge-ai",
+    tools: ["Reactjs", "TailwindCSS", "Firebase", "Gemini api"],
+    status: "In-progress",
+  },
+  {
     projectName: "Quid",
     imgUrl: "https://res.cloudinary.com/daiio9iqt/image/upload/v1715882380/projects/Quid_sxggsi.svg",
     desc: "Quid is a Nextjs themed landing page for a fintech mobile app, it is hooked with a headless CMS(prismic) for the sites data. This template can be used to supercharge your landing page build.",
