1e09061d3b01d8e119d0cfa2315aca64e5133f6a
codejesse <jessebeke10@gmail.com>
2025-03-08 18:22:46 +0100

    footer year fix

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -6,10 +6,11 @@ import { XIcon } from "../icons/XIcon";
 // work on footer
 // use this https://dribbble.com/shots/23531329-kawsar-design-2024
 const Footer = () => {
+  const currentYear = new Date().getFullYear();
   return (
     <div className="flex justify-center mt-10 p-4">
       <div className="text-ceneter p-4 text-gray-500 text-sm lg:text-md">
-        ©2024 Jesse Beke • All Rights Reserved.
+        ©{currentYear} Jesse Beke • All Rights Reserved.
       </div>
       <div className="hidden flex-row gap-2">
         <GitHubIcon className="w-10 my-auto text-white" />
